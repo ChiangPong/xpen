0f1d691d8d9c0aaab66ffa0b62d6e815219b5a27
ChiangPong <chancyngosi9@gmail.com>
2024-06-09 17:39:46 +0200

    vercel push

--- src/lib/general/sideBar.svelte
+++ src/lib/general/sideBar.svelte
@@ -2,17 +2,24 @@
 import { LightSwitch } from '@skeletonlabs/skeleton';
 import { getDrawerStore } from "@skeletonlabs/skeleton";
 import { page } from '$app/stores';
+import { getContext } from 'svelte';
 
 const drawerStore = getDrawerStore();
 
 function drawerClose(): void {
 	drawerStore.close();
 }
+
+// Retrieve user store from context
+const theme = getContext('theme');
+
+let bgColor: string = `bg-${$theme}-400-500-token`;
+
 </script>
 
 <nav class="h-full list-nav p-4 flex flex-col justify-between border-r border-surface-100-800-token">
 	<ul>
-		<li><a href="/dashboard" class="{$page.url.pathname === '/dashboard' ? 'bg-secondary-400-500-token' : ''}" on:click={drawerClose}>Home</a></li>
+		<li><a href="/dashboard" class="{$page.url.pathname === '/dashboard' ? bgColor : ''}" on:click={drawerClose}>Home</a></li>
 	</ul>
 
 	<div class="flex justify-end">
